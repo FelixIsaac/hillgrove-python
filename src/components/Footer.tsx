499c605cd1d32895aa500d1129e4267abb9c595c
Felix Isaac <limfelix16@gmail.com>
2021-06-28 02:03:29 +0800

    update(footer): improve colours

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,36 +1,49 @@
 import React from "react";
 import { Image, Box, Text, Grid, UnorderedList, ListItem, Link, Badge } from "@chakra-ui/react"
+import { FiExternalLink } from 'react-icons/fi';
+import { ColorModeSwitcher } from "./ColorModeSwitcher";
+
+const Icon = () => <FiExternalLink style={{ display: 'inline' }}/>
 
 const Footer = () => {
     return (
         <section className="footer">
-            <Box bg="#ECD799" w="100%" p={4} style={{ borderRadius: "10px 10px 0px 0px" }}>
-                <Grid templateColumns="repeat(3, 1fr)" gap={6}>
+            <Box bg="green.50" w="100%" p={4} style={{ borderRadius: "10px 10px 0px 0px" }}>
+                <Grid templateColumns="repeat(4, 1fr)" gap={6}>
                     <Image src="./images/hillgrove_logo.png" width="236px" height="95px" />
                     <Box w="100%" h="10">
-                        <Text fontWeight="Bold" fontSize="18">Free resources</Text>
-                        <UnorderedList>
+                        <Text fontWeight="Bold" fontSize="18" color="black">Free resources</Text>
+                        <UnorderedList color="black">
                             <ListItem>
-                                <Link color="teal.500" href="https://freecodecamp.org" isExternal>freeCodeCamp</Link>
+                                <Link color="blue.600" href="https://freecodecamp.org" isExternal>freeCodeCamp <Icon /></Link>
                             </ListItem>
                             <ListItem>
-                                <Link color="teal.500" href="https://introtopython.org" isExternal>Intro to Python</Link>
+                                <Link color="blue.600" href="https://introtopython.org" isExternal>Intro to Python <Icon /></Link>
                             </ListItem>
                             <ListItem>
-                                <Link color="teal.500" href="https://kaggle.com" isExternal>Kaggle</Link>
+                                <Link color="blue.600" href="https://kaggle.com" isExternal>Kaggle <Icon /></Link>
                             </ListItem>
                         </UnorderedList>
                     </Box>
                     <Box w="100%" h="10">
-                        <Text fontWeight="Bold" fontSize="18">Support channels</Text>
-                        <UnorderedList>
+                        <Text fontWeight="Bold" fontSize="18" color="black">Support channels</Text>
+                        <UnorderedList color="black">
                             <ListItem>
-                                <Link color="teal.500" href="" isExternal>WhatsApp <Badge colorScheme="red" variant="outline">unavailable</Badge></Link>
+                                <Link color="blue.600" href="" isExternal>
+                                    WhatsApp  <Icon />
+                                    {" "}<Badge colorScheme="red" variant="outline">unavailable</Badge>
+                                </Link>
                             </ListItem>
                             <ListItem>
-                                <Link color="teal.500" href="" isExternal>Telegram <Badge colorScheme="red" variant="outline">unavailable</Badge></Link>
+                                <Link color="blue.600" href="" isExternal>
+                                    Telegram  <Icon />
+                                    {" "}<Badge colorScheme="red" variant="outline">unavailable</Badge>
+                                </Link>
                             </ListItem>
                         </UnorderedList>
+                    </Box>
+                    <Box w="100%" h="10" fontWeight="Bold" fontSize="18" color="black">
+                        Dark mode toggle: <ColorModeSwitcher justifySelf="flex-end" />
                     </Box>
                 </Grid>
             </Box>
