320a04c6c40c8261f03bef1b510dbbb3108c0040
Felix Isaac <limfelix16@gmail.com>
2021-07-30 00:05:06 +0800

    update(session 1): type conversion section

--- markdown/session-1/Type-Conversion.md
+++ markdown/session-1/Type-Conversion.md
@@ -9,7 +9,7 @@ False # booleans
 "{user.firstName}" # strings
 ```
 
-There are times you want convert the value of a variable from one type to another. Let make an input function that takes your birth year and prints your age onto the console.
+There are times you want convert the value of a variable from one type to another. Let's make an input function that takes your birth year and prints your age onto the console.
 
 ```py
 birth_year = input("Enter your birth year: ")
@@ -64,3 +64,73 @@ Let's speak out the code,
 - On the first line. We call the `input()` function. The function returns a string, the string gets assigned to `birth_year`.
 - On the second line. We pass `birth_year` (a string) to the `int()` function. The `int()` function returns the numerical representation of `birth_year`. Which is then subtracted from the current year (2021) to get the age. This age number gets assigned to the variable `age`
 - On the third line, we `output/print` the age.
+
+
+## Integers examples
+
+```py
+x = int(1)   # x will be 1
+y = int(2.8) # y will be 2
+z = int("3") # z will be 3
+```
+
+## Floats examples
+
+```py
+x = float(1)     # x will be 1.0
+y = float(2.8)   # y will be 2.8
+z = float("3")   # z will be 3.0
+w = float("4.2") # w will be 4.2
+```
+
+## Strings examples
+
+```py
+x = str("s1") # x will be 's1'
+y = str(2)    # y will be '2'
+z = str(3.0)  # z will be '3.0'
+```
+
+## Practice
+
+Let's make a simple calculator that adds two numbers. We want the calculator to do
+
+- User inputs first number
+- User inputs second number
+- Add the two numbers
+- Prints the result
+
+```py
+first_num = input('First number: ')
+second_num = input('Second number: ')
+
+result = first_num + second_num
+
+print(result)
+```
+
+Let's run the following code... You'll notice that it literally joins both strings together, so if your first number is `20` and your second number is `30`, the program outputs `2030` instead of `50`. This is because Python is concatenating both strings, which is derived from both input functions. To solve this, simple convert these strings into numbers.
+
+```py
+first_num = int(input('First number: '))
+second_num = int(input('Second number: '))
+
+result = first_num + second_num
+
+print(result)
+```
+
+Running the updated code again, we can see that we get `50` now. But what if we wanted to include decimals? Well, let's say the first number `15.8` and the second `20.2`. Since in the code you are converting the string `"15.8"` into an integer, which is a whole number, **Python will literally remove the decimals, without rounding up to rounding down.** To fix this, we need to convert the strings into floats (numbers with decimals) instead of integers.
+
+```py
+first_num = float(input('First number: '))
+second_num = float(input('Second number: '))
+
+result = first_num + second_num
+
+print(result)
+```
+
+## Conclusion
+
+Type conversion is important in Python and other programming languages. There are times you need to convert the type of variable to a different type.
--- src/sessions/session-1/TypeConversion.tsx
+++ src/sessions/session-1/TypeConversion.tsx
@@ -80,6 +80,83 @@ const TypeConversion = () => {
                     <li>On the third line, we <Kbd>output/print</Kbd> the age.</li>
                 </ul>
             </Text>
+            <Text>
+                <Heading as="h2">Integers examples</Heading>
+                <CodeSnippet code={[
+                    "x = int(1)   # x will be 1",
+                    "y = int(2.8) # y will be 2",
+                    "z = int('3') # z will be 3"
+                ]} />
+            </Text>
+            <Text>
+                <Heading as="h2">Floats examples</Heading>
+                 <CodeSnippet code={[
+                    `x = float(1)     # x will be 1.0`,
+                    `y = float(2.8)   # y will be 2.8`,
+                    `z = float("3")   # z will be 3.0`,
+                    `w = float("4.2") # w will be 4.2`
+                ]} />
+            </Text>
+            <Text>
+                <Heading as="h2">Strings examples</Heading>
+                 <CodeSnippet code={[
+                    `x = str("s1") # x will be 's1'`,
+                    `y = str(2)    # y will be '2'`,
+                    `z = str(3.0)  # z will be '3.0'`
+                ]} />
+            </Text>
+            <Text>
+                <Heading as="h2">SPractice</Heading>
+                Let's make a simple calculator that adds two numbers. We want the calculator to do
+                <ul>
+                   <li>User inputs first number</li>
+                   <li>User inputs second number</li>
+                   <li>Add the two numbers</li>
+                   <li>Prints the result</li>
+                </ul>
+                <CodeSnippet code={[
+                    `first_num = input('First number: ')`,
+                    `second_num = input('Second number: ')`,
+                    ``,
+                    `result = first_num + second_num`,
+                    ``,
+                    `print(result)`
+                ]}/>
+            </Text>
+            <Text>
+                Let's run the following code... You'll notice that it literally joins both strings together,
+                so if your first number is <Kbd>20</Kbd> and your second number is <Kbd>30</Kbd>, the program outputs <Kbd>2030</Kbd>
+                instead of <Kbd>50</Kbd>. This is because Python is concatenating both strings, which is derived from both input functions.
+                To solve this, simple convert these strings into numbers.
+                <CodeSnippet code={[
+                    `first_num = int(input('First number: '))`,
+                    `second_num = int(input('Second number: '))`,
+                    ``,
+                    `result = first_num + second_num`,
+                    ``,
+                    `print(result)`
+                ]}/>
+            </Text>
+            <Text>
+                Running the updated code again, we can see that we get <Kbd>50</Kbd> now. But what if we wanted to include decimals?
+                Well, let's say the first number <Kbd>15.8</Kbd> and the second <Kbd>20.2</Kbd>. Since in the code you
+                are converting the string <Kbd>"15.8"</Kbd> into an integer, which is a whole number, <strong>Python will literally
+                remove the decimals, without rounding up to rounding down.</strong> To fix this, we need to convert the strings
+                into floats (numbers with decimals) instead of integers.
+                <CodeSnippet code={[
+                    `first_num = float(input('First number: '))`,
+                    `second_num = float(input('Second number: '))`,
+                    ``,
+                    `result = first_num + second_num`,
+                    ``,
+                    `print(result)`
+                ]}/>
+            </Text>
+            <Text>
+                <Heading as="h2">Conclusion</Heading>
+                Type conversion is important in Python and other programming languages.
+                There are times you need to convert the type of variable to a different type.
+            </Text>
         </Box>
     )
 }
